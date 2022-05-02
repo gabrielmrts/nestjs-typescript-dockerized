97e650a335c0da7fd6cbe83e705189f4a5a6e450
Gabriel <gml@orbmusic.com.br>
2022-05-02 15:48:15 -0300

    Small corrections

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API Description')
+    .setVersion('1.0')
+    .addTag('aaa')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
   await app.listen(3000);
 }
 
